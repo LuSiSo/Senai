a757e456e549b1dbfcf90be1ce25d7083bd1bae7
Julia Zanirato <juliazaniratoo@gmail.com>
2026-05-27 08:39:49 -0300

    Itens
    Added enums for item rarity and type, and created ItemData class.

--- Assets/Scripts/Itens/ItemData.cs
+++ Assets/Scripts/Itens/ItemData.cs
@@ -1,1 +1,21 @@
-Se vira julia
+using UnityEngine;
+
+public enum Raridade { Comum, Raro, Lendario }
+public enum TipoItem
+{
+    FragmentoSombrio,
+    MedalhaoFuriaAntiga,
+    RelicarioIra,
+    AmuletoGuerreiroAntigo,
+    AnelEclipse
+}
+
+[CreateAssetMenu(fileName = "Novo Item", menuName = "Inventario/Item")]
+public class ItemData : ScriptableObject
+{
+    public string nomeItem;
+    [TextArea] public string descricao;
+    public Raridade raridade;
+    public TipoItem tipoItem;
+    public Sprite icone; // Útil se for criar um inventário visual depois
+}
