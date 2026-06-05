f125488b32d55888f57697ba27fb1ca6f5dc1110
Lucas Sousa <silveriodesousalucas6@gmail.com>
2026-06-04 21:11:33 -0300

    Add Parallax script for camera movement effect

---
+++ Assets/Scripts/Systems/Parallex.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public Transform cameraTransform;
+    public float parallaxEffect = 0.5f;
+
+    private float startPosX;
+
+    void Start()
+    {
+        startPosX = transform.position.x;
+    }
+
+    void Update()
+    {
+        float distance = cameraTransform.position.x * parallaxEffect;
+
+        transform.position = new Vector3(
+            startPosX + distance,
+            transform.position.y,
+            transform.position.z
+        );
+    }
+}
